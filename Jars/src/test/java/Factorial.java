71b2807a768fa21442387a3f218a2c49962a5f94
Prabhakaran <sarav1006@gmail.com>
2020-09-29 10:01:16 +0530

    Initial Commit

---
+++ Jars/src/test/java/Factorial.java
@@ -0,0 +1,24 @@
+
+public class Factorial {
+	
+	// N! = N x (N-1) x (N-2) x ....x 2 x 1
+			// 5! = N x (5-1) x (5-2) x 2 x 1
+	
+	public static int factorial (int N) {
+		if (N <= 1)
+			return 1;
+		else return (N * factorial(N-1));
+	}
+		
+	public static void main(String[] args) {
+
+		System.out.println(4);
+		
+		
+			
+		
+			
+	}
+
+
+}
